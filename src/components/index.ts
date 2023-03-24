5b97045c98e10c9a00e5ea90f262271191c5297e
C5m7b4 <mbedingfield@dcrpos.com>
2023-03-24 15:58:16 -0500

    take out all components except the button

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,98 +4,100 @@ import {
   NormalButtonSize,
   ButtonProps,
 } from './Button';
-import { SelectFieldProps, SelectField, SelectTheme } from './SelectField';
-import {
-  TextTheme,
-  TextFieldType,
-  TextFieldAutoComplete,
-  TextFieldProps,
-  TextField,
-} from './TextField';
-import { RippleTypes, RippleButtonProps, RippleButton } from './RippleButton';
-import {
-  OutlineButton,
-  OutlineButtonProps,
-  OutlineButtonTypes,
-} from './OutlineButton';
-import { CodeDisplay, CodeDisplayProps } from './CodeDisplay';
-import {
-  CircularProgressBar,
-  CircularProgressProps,
-} from './CircularProgressBar';
-import { ToggleSwitch, ToggleSwitchProps } from './ToggleSwitch';
-import {
-  Tooltip,
-  TooltipPosition,
-  TooltipTheme,
-  TooltipProps,
-} from './Tooltip';
-import {
-  MultiSelect,
-  MultiSelectProps,
-  MultiSelectPortal,
-  MultiSelectPortalProps,
-} from './MultiSelect';
-import { PSM, PSMProps } from './PSM';
-import {
-  Calendar,
-  CalendarProps,
-  EventListProps,
-  EventList,
-  EventViewerProps,
-  EventViewer,
-} from './Calendar';
 
-import {
-  DatePicker,
-  DatePickerProps,
-  DatePickerContentProps,
-  DatePickerContents,
-} from './DatePicker';
+export { Button, NormalButtonType, NormalButtonSize, ButtonProps };
+// import { SelectFieldProps, SelectField, SelectTheme } from './SelectField';
+// import {
+//   TextTheme,
+//   TextFieldType,
+//   TextFieldAutoComplete,
+//   TextFieldProps,
+//   TextField,
+// } from './TextField';
+// import { RippleTypes, RippleButtonProps, RippleButton } from './RippleButton';
+// import {
+//   OutlineButton,
+//   OutlineButtonProps,
+//   OutlineButtonTypes,
+// } from './OutlineButton';
+// import { CodeDisplay, CodeDisplayProps } from './CodeDisplay';
+// import {
+//   CircularProgressBar,
+//   CircularProgressProps,
+// } from './CircularProgressBar';
+// import { ToggleSwitch, ToggleSwitchProps } from './ToggleSwitch';
+// import {
+//   Tooltip,
+//   TooltipPosition,
+//   TooltipTheme,
+//   TooltipProps,
+// } from './Tooltip';
+// import {
+//   MultiSelect,
+//   MultiSelectProps,
+//   MultiSelectPortal,
+//   MultiSelectPortalProps,
+// } from './MultiSelect';
+// import { PSM, PSMProps } from './PSM';
+// import {
+//   Calendar,
+//   CalendarProps,
+//   EventListProps,
+//   EventList,
+//   EventViewerProps,
+//   EventViewer,
+// } from './Calendar';
 
-export {
-  Button,
-  NormalButtonType,
-  NormalButtonSize,
-  ButtonProps,
-  SelectFieldProps,
-  SelectField,
-  SelectTheme,
-  TextTheme,
-  TextFieldType,
-  TextFieldAutoComplete,
-  TextFieldProps,
-  TextField,
-  RippleTypes,
-  RippleButtonProps,
-  RippleButton,
-  OutlineButton,
-  OutlineButtonProps,
-  OutlineButtonTypes,
-  CodeDisplay,
-  CodeDisplayProps,
-  CircularProgressBar,
-  CircularProgressProps,
-  ToggleSwitch,
-  ToggleSwitchProps,
-  Tooltip,
-  TooltipPosition,
-  TooltipTheme,
-  TooltipProps,
-  MultiSelect,
-  MultiSelectProps,
-  MultiSelectPortal,
-  MultiSelectPortalProps,
-  PSM,
-  PSMProps,
-  Calendar,
-  CalendarProps,
-  EventListProps,
-  EventList,
-  EventViewerProps,
-  EventViewer,
-  DatePicker,
-  DatePickerProps,
-  DatePickerContentProps,
-  DatePickerContents,
-};
+// import {
+//   DatePicker,
+//   DatePickerProps,
+//   DatePickerContentProps,
+//   DatePickerContents,
+// } from './DatePicker';
+
+// export {
+//   Button,
+//   NormalButtonType,
+//   NormalButtonSize,
+//   ButtonProps,
+//   SelectFieldProps,
+//   SelectField,
+//   SelectTheme,
+//   TextTheme,
+//   TextFieldType,
+//   TextFieldAutoComplete,
+//   TextFieldProps,
+//   TextField,
+//   RippleTypes,
+//   RippleButtonProps,
+//   RippleButton,
+//   OutlineButton,
+//   OutlineButtonProps,
+//   OutlineButtonTypes,
+//   CodeDisplay,
+//   CodeDisplayProps,
+//   CircularProgressBar,
+//   CircularProgressProps,
+//   ToggleSwitch,
+//   ToggleSwitchProps,
+//   Tooltip,
+//   TooltipPosition,
+//   TooltipTheme,
+//   TooltipProps,
+//   MultiSelect,
+//   MultiSelectProps,
+//   MultiSelectPortal,
+//   MultiSelectPortalProps,
+//   PSM,
+//   PSMProps,
+//   Calendar,
+//   CalendarProps,
+//   EventListProps,
+//   EventList,
+//   EventViewerProps,
+//   EventViewer,
+//   DatePicker,
+//   DatePickerProps,
+//   DatePickerContentProps,
+//   DatePickerContents,
+// };
